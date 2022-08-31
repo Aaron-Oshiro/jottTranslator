0192692eb86ff179442bf6d96bc9a939df83b468
Aaron-Oshiro <aaron.oshiro@atscale.com>
2022-08-31 17:00:23 -0400

    Added basic code to tokenizer

--- src/JottTokenizer.java
+++ src/JottTokenizer.java
@@ -6,6 +6,9 @@
  **/
 
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class JottTokenizer {
 
@@ -16,6 +19,28 @@ public class JottTokenizer {
      * @return an ArrayList of Jott Tokens
      */
     public static ArrayList<Token> tokenize(String filename){
-		return null;
+
+		ArrayList<Token> tokens = new ArrayList<>();
+    	try {
+			Scanner jottFile = new Scanner(new File(filename));
+			int lineNumber = 0;		// I think this is a good way of knowing which line we are on
+			while (jottFile.hasNextLine()) {
+				lineNumber++;
+				String jottLine = jottFile.nextLine();
+				ArrayList<Token> newTokens = createTokensPerLine(jottLine, filename, lineNumber);
+				tokens.addAll(newTokens);
+				System.out.println(jottLine);		// test sout
+
+			}
+		} catch (FileNotFoundException e) {
+			System.err.println("File not found");
+			return null;
+		}
+
+    	return tokens;
+	}
+
+	private static ArrayList<Token> createTokensPerLine(String jottLine, String fileName, int lineNum) {
+    	return new ArrayList<>();
 	}
 }

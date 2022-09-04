02ec5109a378a38d9c6c6ef64788e827c8aa8bd8
Aaron-Oshiro <aaron.oshiro@atscale.com>
2022-09-03 23:00:04 -0400

    Added number, id/keyword, !, and " cases

--- src/JottMain.java
+++ src/JottMain.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class JottMain {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         ArrayList<Token> hi = new ArrayList<Token>(JottTokenizer.tokenize("input.jott"));
         System.out.println(hi);
     }

fc1dc17ee1905a0bb23c6510609c664ebf7523c7
rzv09 <rzv090701@gmail.com>
2022-09-28 15:37:40 -0400

    Aaron we love you uwu

--- src/Node.java
+++ src/Node.java
@@ -6,6 +6,48 @@ public class Node {
     private Node parent;
     private ArrayList<Token> children;
 
+    public enum nodeType {
+        PROGRAM,
+        FUNCTION_LIST,
+        FUNCTION_DEF,
+        FUNC_DEF_PARAMS,
+        FUNC_DEF_PARAMS_T,
+        BODY_STMT,
+        RETURN_STMT,
+        BODY,
+        END_STMT,
+        IF_STMT,
+        ELSE,
+        ELSEIF_LST,
+        WHILE_LOOP,
+        CHAR,
+        L_CHAR,
+        U_CHAR,
+        DIGIT,
+        SIGN,
+        ID,
+        STMT,
+        FUNC_CALL,
+        PARAMS,
+        PARAMS_T,
+        EXPR,
+        TYPE,
+        FUNCTION_RETURN,
+        VAR_DEC,
+        ASMT,
+        OP,
+        REL_OP,
+        DBL,
+        D_EXPR,
+        BOOL,
+        B_EXPR,
+        INT,
+        I_EXPR,
+        STR_LITERAL,
+        STR,
+        S_EXPR
+    }
+
     public Node(String data, Node parent) {
         this.data = data;
         this.parent = parent;

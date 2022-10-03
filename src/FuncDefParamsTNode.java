37c5b3f0f557b8f9630e2a71b8f473e0f29d9c2a
rzv09 <rzv090701@gmail.com>
2022-10-02 22:55:39 -0400

    FuncDefParamsNode & FuncDefParamsTNode

--- src/FuncDefParamsTNode.java
+++ src/FuncDefParamsTNode.java
@@ -1,46 +1,37 @@
 import java.util.ArrayList;
-
 /**
  * FuncDefParamsNode
  *
  * @author Raman Zatsarenko
  */
-public class FuncDefParamsNode implements JottTree{
-    private boolean hasFuncDefParams;
+public class FuncDefParamsTNode implements JottTree {
+
+    private boolean hasParamsT;
     private IdNode id;
     private TypeNode type;
     private FuncDefParamsTNode funcDefParamsT;
 
-    public FuncDefParamsNode(ArrayList<Token> tokens) throws Exception {
-        // func params is [], so list is empty
-        if(tokens.get(0).getToken().equals("]")) {
-            hasFuncDefParams = false;
-        }
+    public FuncDefParamsTNode(ArrayList<Token> tokens) throws Exception {
+        if(!tokens.get(0).getToken().equals(",")) hasParamsT = false;
         else {
-            hasFuncDefParams = true;
-            // The bracket check should be in FuncDefNode
-            //if(!tokens.get(0).getToken().equals("[")) {
-            //    throw new Exception("Token "+ tokens.get(0).getToken() + "cannot be parsed into a [ at line " + tokens.get(0).getLineNum());
-            //}
-            // remove [
-            //tokens.remove(0);
+            hasParamsT = true;
+            // remove comma
+            tokens.remove(0);
             id = new IdNode(tokens);
             if(!tokens.get(0).getToken().equals(":")) {
                 throw new Exception("Token "+ tokens.get(0).getToken() + "cannot be parsed into a : at line " + tokens.get(0).getLineNum());
             }
-            // remove ":"
             tokens.remove(0);
             type = new TypeNode(tokens);
             funcDefParamsT = new FuncDefParamsTNode(tokens);
-
         }
     }
 
     @Override
     public String convertToJott() {
-        if (!hasFuncDefParams) return "";
+        if(!hasParamsT) return "";
         else {
-            return id.convertToJott() + ":" + type.convertToJott() + funcDefParamsT.convertToJott();
+            return "," + id.convertToJott() + ":" + type.convertToJott() + funcDefParamsT.convertToJott();
         }
     }
 
@@ -63,4 +54,5 @@ public class FuncDefParamsNode implements JottTree{
     public boolean validateTree() {
         return false;
     }
+
 }

3d17f600a09122efec0475358da27aa775a3b106
Drysco <makerofmyths@gmail.com>
2022-10-03 19:31:37 -0400

    Dbl Node

--- src/DblNode.java
+++ src/DblNode.java
@@ -6,10 +6,32 @@ public class DblNode implements JottTree{
 
     //Digit
 
+    private Double value;
+
     // I AM WAITING FOR SIGN AND DIGIT BEFORE I DO THIS ONE
 
     public DblNode(ArrayList<Token> tokens)throws Exception{
         Token tokenToCheck = tokens.get(0);
+
+    try
+    {
+        value = Double.parseDouble(tokenToCheck.getToken());
+        
+        if(!tokenToCheck.getToken().contains(".")){
+            throw new Exception("Token "+ tokenToCheck.toString() + "cannot be parsed into a Double (Did not have decimal) at line " + tokenToCheck.getLineNum());
+        }
+        else{
+
+        tokens.remove(0);
+        }
+    }
+    catch(Exception e)
+    {
+        //not a double
+        //throw error if we were expecting a double
+
+        throw new Exception("Token "+ tokenToCheck.toString() + "cannot be parsed into a Double at line " + tokenToCheck.getLineNum());
+    }
 
       
     }

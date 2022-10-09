f118f70fe88ab9f0cd65d98e442c556cacb43f7e
Camille Decker <cid3081@g.rit.edu>
2022-10-09 17:49:36 -0400

    edit at line 22: edited error statement to read - cannot be parsed into expression at... -

--- src/ExprNode.java
+++ src/ExprNode.java
@@ -7,6 +7,10 @@ public class ExprNode implements JottTree {
     private boolean exprFlag;
     private IdNode id;
     private boolean idFlag;
+    private ValueNode value;
+    private boolean valueFlag;
+    private opNode op;
+    private boolean opFlag;
     private FuncCallNode funcCall;
     private boolean funcCallFlag;
 
@@ -14,19 +18,51 @@ public class ExprNode implements JottTree {
         Token t0 = tokens.get(0);
         TokenType tt0 = (tokens.get(0).getTokenType());
         if (tt0 != TokenType.ID_KEYWORD && tt0 != TokenType.NUMBER && tt0 != TokenType.STRING) {
-            throw new Exception("Token " + t0.getToken() + "cannot be parsed into a [ at line " + t0.getLineNum());
+            throw new Exception(
+                    "Token " + t0.getToken() + "cannot be parsed into an Expression at line " + t0.getLineNum());
         }
-        Token t1 = tokens.get(1);
+        // Token t1 = tokens.get(1);
         TokenType tt1 = (tokens.get(1).getTokenType());
         if (tt1 == TokenType.MATH_OP || tt1 == TokenType.REL_OP) {
-            opNode(tokens);
+            if (tt0 == TokenType.NUMBER || tt0 == TokenType.STRING) {
+                value = new ValueNode(tokens);
+                valueFlag = true;
+            } else {
+                id = new IdNode(tokens);
+                idFlag = true;
+            }
+            op = new opNode(tokens);
+            opFlag = true;
+
+            expr = new ExprNode(tokens);
+            exprFlag = true;
+
+        } else {
+            if (tt0 == TokenType.ID_KEYWORD) {
+                id = new IdNode(tokens);
+                idFlag = true;
+            }
+
+            if ((tt0 == TokenType.STRING) || (tt0 == TokenType.NUMBER)) {
+                value = new ValueNode(tokens);
+                valueFlag = true;
+            }
         }
 
     }
 
     @Override
     public String convertToJott() {
-        return null;
+        if (opFlag) {
+            if (idFlag) {
+                return id.convertToJott() + op.convertToJott() + expr.convertToJott();
+            }
+            return value.convertToJott() + op.convertToJott() + expr.convertToJott();
+        }
+        if (idFlag) {
+            return id.convertToJott();
+        }
+        return value.convertToJott();
     }
 
     @Override

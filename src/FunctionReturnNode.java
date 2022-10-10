dc50762346761718e3ee5116ecd689daeacd7276
Camille Decker <cid3081@g.rit.edu>
2022-10-10 11:20:45 -0400

    FunctionReturnNode.java: added initial implementation to Function Return node

--- src/FunctionReturnNode.java
+++ src/FunctionReturnNode.java
@@ -1,12 +1,28 @@
 import java.util.ArrayList;
 
-public class FunctionReturnNode implements JottTree{
-    public FunctionReturnNode(ArrayList<Token> tokens) throws Exception {
+public class FunctionReturnNode implements JottTree {
 
+    private TypeNode returnType;
+    private boolean returnTypeFlag = false;
+    private boolean voidFlag = false;
+
+    public FunctionReturnNode(ArrayList<Token> tokens) throws Exception {
+        String t0 = tokens.get(0).getToken();
+        if (t0 == "void") {
+            voidFlag = true;
+        } else {
+            returnTypeFlag = true;
+            returnType = new TypeNode(tokens);
+        }
     }
+
     @Override
     public String convertToJott() {
-        return null;
+        if (voidFlag) {
+            return "Void";
+        } else {
+            return returnType.convertToJott();
+        }
     }
 
     @Override

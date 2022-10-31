9418e16e5bb4c89e771e44faf7f2f448266ab999
Camille Decker <cid3081@g.rit.edu>
2022-10-31 14:23:27 -0400

    FunctionListNode.java: lines 59-62: changed validateTree to return true if the node has no function definition; otherwise, validate its functionDefinitionNode AND its functionListNode

--- src/FunctionListNode.java
+++ src/FunctionListNode.java
@@ -1,38 +1,39 @@
 import java.util.*;
-public class FunctionListNode implements JottTree{
 
+public class FunctionListNode implements JottTree {
 
     private FunctionDefNode functionDefNode;
     private FunctionListNode functionListNode;
     private boolean hasFunctionDefinition;
 
-    public FunctionListNode(ArrayList<Token> tokens, HashMap<String, FunctionDefNode> functionTable) throws Exception{
-        
-        if(tokens.size() == 0){
-            //case where func list = epsilon
-            hasFunctionDefinition = false;
-        }
-        else{
-            //case where func list is not epsilon. It is then func def and another func list
-            hasFunctionDefinition = true;
-        functionDefNode = new FunctionDefNode(tokens);
-        if (functionTable.containsKey(functionDefNode.getIdNode().convertToJott())){
-            throw new Exception("Semantic Error: " + functionDefNode.getIdNode().convertToJott() + " function is already defined");
-        } else {
-            functionTable.put(functionDefNode.getIdNode().convertToJott(), functionDefNode);
-        }
+    public FunctionListNode(ArrayList<Token> tokens, HashMap<String, FunctionDefNode> functionTable) throws Exception {
 
-        functionListNode = new FunctionListNode(tokens, functionTable);
+        if (tokens.size() == 0) {
+            // case where func list = epsilon
+            hasFunctionDefinition = false;
+        } else {
+            // case where func list is not epsilon. It is then func def and another func
+            // list
+            hasFunctionDefinition = true;
+            functionDefNode = new FunctionDefNode(tokens);
+            if (functionTable.containsKey(functionDefNode.getIdNode().convertToJott())) {
+                throw new Exception("Semantic Error: " + functionDefNode.getIdNode().convertToJott()
+                        + " function is already defined");
+            } else {
+                functionTable.put(functionDefNode.getIdNode().convertToJott(), functionDefNode);
+            }
+
+            functionListNode = new FunctionListNode(tokens, functionTable);
         }
 
     }
-    
+
     @Override
     public String convertToJott() {
-        if(!hasFunctionDefinition){
+        if (!hasFunctionDefinition) {
             return "";
         }
-        return functionDefNode.convertToJott() + "\n" +  functionListNode.convertToJott();
+        return functionDefNode.convertToJott() + "\n" + functionListNode.convertToJott();
     }
 
     @Override
@@ -55,10 +56,10 @@ public class FunctionListNode implements JottTree{
 
     @Override
     public boolean validateTree() {
-        // TODO Auto-generated method stub
-        return false;
+        if (!hasFunctionDefinition) {
+            return true;
+        }
+        return ((functionDefNode.validateTree()) && (functionListNode.validateTree()));
     }
 
-    
-    
 }

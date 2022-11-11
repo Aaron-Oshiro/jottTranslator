51cfac6133049e2553dcb49cdd80fa2ea566ec40
Camille Decker <cid3081@g.rit.edu>
2022-11-11 11:16:05 -0500

    AsmtNode.java: lines 98-106: initial implementation of convertToC. ISSUE! ISSUE! type is a string in this class, so convertToC cannot be called on it. either must be changed to a TypeNode or will have to add a helper function to convert

--- src/AsmtNode.java
+++ src/AsmtNode.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 /**
  * @author Raman Zatsarenko
  */
-public class AsmtNode implements JottTree{
+public class AsmtNode implements JottTree {
     private IdNode id;
 
     private DExprNode dExpr;
@@ -21,7 +21,7 @@ public class AsmtNode implements JottTree{
     private EndStmtNode endStmt;
 
     // probably need lookahead of 1 or 2?
-    public AsmtNode(ArrayList<Token> tokens, HashMap<String, IdNode> symbolTable) throws Exception{
+    public AsmtNode(ArrayList<Token> tokens, HashMap<String, IdNode> symbolTable) throws Exception {
         String currentTokenStr = tokens.get(0).getToken();
         if (types.contains(currentTokenStr)) {
             hasType = true;
@@ -30,10 +30,13 @@ public class AsmtNode implements JottTree{
             // remove type token
             tokens.remove(0);
             // <id>
-            //TODO add a check here - make sure the id node has the token type of ID/Keyword!
+            // TODO add a check here - make sure the id node has the token type of
+            // ID/Keyword!
             id = new IdNode(tokens);
             if (!tokens.get(0).getToken().equals("=")) {
-                throw new Exception("Syntax Error: Token "+ tokens.get(0).getToken() + " cannot be parsed into a '=' at " + tokens.get(0).getFilename() + "  line " + tokens.get(0).getLineNum());
+                throw new Exception(
+                        "Syntax Error: Token " + tokens.get(0).getToken() + " cannot be parsed into a '=' at "
+                                + tokens.get(0).getFilename() + "  line " + tokens.get(0).getLineNum());
             }
             tokens.remove(0); // remove =
 
@@ -47,23 +50,26 @@ public class AsmtNode implements JottTree{
                 symbolTable.put(id.convertToJott(), id);
             }
 
-        // <id>
-        // <s_expr>
-            //sExpr = new StrExprNode(tokens);
-        // <id>
-        // <i_expr>
-            //iExpr = new IExprNode(tokens);
-        // <id>
-        // <b_expr>
-            //bExpr = new BExprNode(tokens);
+            // <id>
+            // <s_expr>
+            // sExpr = new StrExprNode(tokens);
+            // <id>
+            // <i_expr>
+            // iExpr = new IExprNode(tokens);
+            // <id>
+            // <b_expr>
+            // bExpr = new BExprNode(tokens);
 
         }
         // <id> = <*_expr><end_stmt>
         else {
-            //TODO add a check here - make sure the id node has the token type of ID/Keyword!
+            // TODO add a check here - make sure the id node has the token type of
+            // ID/Keyword!
             id = new IdNode(tokens);
             if (!tokens.get(0).getToken().equals("=")) {
-                throw new Exception("Syntax Error: Token "+ tokens.get(0).getToken() + " cannot be parsed into a '=' at "  + tokens.get(0).getFilename() + " line " + tokens.get(0).getLineNum());
+                throw new Exception(
+                        "Syntax Error: Token " + tokens.get(0).getToken() + " cannot be parsed into a '=' at "
+                                + tokens.get(0).getFilename() + " line " + tokens.get(0).getLineNum());
             }
             tokens.remove(0);
 
@@ -77,8 +83,7 @@ public class AsmtNode implements JottTree{
     public String convertToJott() {
         if (hasType) {
             return type + " " + id.convertToJott() + " = " + expr.convertToJott() + endStmt.convertToJott();
-        }
-        else {
+        } else {
             return id.convertToJott() + " = " + expr.convertToJott() + endStmt.convertToJott();
         }
     }
@@ -90,7 +95,15 @@ public class AsmtNode implements JottTree{
 
     @Override
     public String convertToC() {
-        return null;
+        if (hasType) {
+            // ISSUE! because 'type' here is a string and not a typeNode
+            // cannot call convertToC. if this can't change,
+            // will have to include several if statements.
+            // (because the type in C is written differently)
+            return type + " " + id.convertToC() + " = " + expr.convertToC() + endStmt.convertToC();
+        } else {
+            return id.convertToC() + " = " + expr.convertToC() + endStmt.convertToC();
+        }
     }
 
     @Override
@@ -103,28 +116,31 @@ public class AsmtNode implements JottTree{
         // <id> = <expr><end_stmt>
         if (!hasType) {
             if (!symbolTable.containsKey(id.getId())) {
-                System.err.println("Semantic Error: Symbol '" + id + "' was not found in the Symbol Table at File and Line:To be Implemented");
+                System.err.println("Semantic Error: Symbol '" + id
+                        + "' was not found in the Symbol Table at File and Line:To be Implemented");
                 return false;
             } else {
                 // Aaron asked for this
                 // System.out.println(id.getType());
                 id.setType(expr.getType(functionTable, symbolTable));
-                if(id.validateTree(functionTable, symbolTable) && expr.validateTree(functionTable, symbolTable) &&
+                if (id.validateTree(functionTable, symbolTable) && expr.validateTree(functionTable, symbolTable) &&
                         (id.getType().equals(expr.getType(functionTable, symbolTable))))
-                        return true;
-                    else{
-                        System.err.println("Semantic Error: Either id or the expression were invalid, OR the types of the two did not match at File and Line: To be implemented");
-                        return false;
-                    }
+                    return true;
+                else {
+                    System.err.println(
+                            "Semantic Error: Either id or the expression were invalid, OR the types of the two did not match at File and Line: To be implemented");
+                    return false;
+                }
             }
         } else {
-            if(id.validateTree(functionTable, symbolTable) && expr.validateTree(functionTable, symbolTable) &&
+            if (id.validateTree(functionTable, symbolTable) && expr.validateTree(functionTable, symbolTable) &&
                     type.equals(expr.getType(functionTable, symbolTable)))
-                    return true;
-                    else{
-                        System.err.println("Semantic Error: Either id or the expression were invalid, OR the types of the two did not match at File and Line: To be implemented");
-                        return false;
-                    }
+                return true;
+            else {
+                System.err.println(
+                        "Semantic Error: Either id or the expression were invalid, OR the types of the two did not match at File and Line: To be implemented");
+                return false;
+            }
         }
     }
 }

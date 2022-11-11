fc99a1646a4b42a539273c9d719468d0d5ae3fb6
Camille Decker <cid3081@g.rit.edu>
2022-11-11 11:32:49 -0500

    ElseNode.java: lines: 45: 59-62: deleted leftover method stub comment from convertToJott function: initial implementation of converToC

--- src/ElseNode.java
+++ src/ElseNode.java
@@ -1,52 +1,51 @@
 import java.util.*;
-public class ElseNode implements JottTree{
+
+public class ElseNode implements JottTree {
 
     private boolean hasElse;
 
     private BodyNode bodyNode;
 
-    public ElseNode(ArrayList<Token> tokens, HashMap<String, IdNode> symbolTable)throws Exception{
+    public ElseNode(ArrayList<Token> tokens, HashMap<String, IdNode> symbolTable) throws Exception {
         Token tokenToCheck = tokens.get(0);
 
-
-        if(tokenToCheck.getToken().equals("else")){
-            //get rid of the else
+        if (tokenToCheck.getToken().equals("else")) {
+            // get rid of the else
             tokens.remove(0);
             hasElse = true;
 
-            //get rid of lbracket
-            if(!tokens.get(0).getToken().equals("{")){
-                throw new Exception("Syntax Error: Token "+ tokenToCheck.getToken() + " cannot be parsed into a { at " +tokenToCheck.getFilename() + " line " + tokenToCheck.getLineNum());
+            // get rid of lbracket
+            if (!tokens.get(0).getToken().equals("{")) {
+                throw new Exception("Syntax Error: Token " + tokenToCheck.getToken() + " cannot be parsed into a { at "
+                        + tokenToCheck.getFilename() + " line " + tokenToCheck.getLineNum());
             }
             tokens.remove(0);
 
             bodyNode = new BodyNode(tokens, symbolTable);
-             
 
-             //get rid of rbracket
-            if(!tokens.get(0).getToken().equals("}")){
-                throw new Exception("Syntax Error: Token "+ tokenToCheck.getToken() + "cannot be parsed into a } at " +tokenToCheck.getFilename() + " line " + tokenToCheck.getLineNum());
+            // get rid of rbracket
+            if (!tokens.get(0).getToken().equals("}")) {
+                throw new Exception("Syntax Error: Token " + tokenToCheck.getToken() + "cannot be parsed into a } at "
+                        + tokenToCheck.getFilename() + " line " + tokenToCheck.getLineNum());
             }
             tokens.remove(0);
 
-        }
-        else{
-                    hasElse = false;
-                    //TODO handle Empty string case. Do I need to look ahead? I think if the token is not else we assume it is the empty case.
+        } else {
+            hasElse = false;
+            // TODO handle Empty string case. Do I need to look ahead? I think if the token
+            // is not else we assume it is the empty case.
 
-
-
-            //throw new Exception("Token "+ tokenToCheck.toString() + "cannot be parsed into a Else at line " + tokenToCheck.getLineNum());
+            // throw new Exception("Token "+ tokenToCheck.toString() + "cannot be parsed
+            // into a Else at line " + tokenToCheck.getLineNum());
         }
     }
-    
+
     @Override
     public String convertToJott() {
-        // TODO Auto-generated method stub
-        if(!hasElse){
+        if (!hasElse) {
             return "";
         }
-        return "else{"+ bodyNode.convertToJott() + "}";
+        return "else{" + bodyNode.convertToJott() + "}";
     }
 
     @Override
@@ -57,33 +56,34 @@ public class ElseNode implements JottTree{
 
     @Override
     public String convertToC() {
-        // TODO Auto-generated method stub
-        return null;
+        if (!hasElse) {
+            return "";
+        }
+        return "else{" + bodyNode.convertToC() + "}";
     }
 
     @Override
     public String convertToPython(int t) {
-        if (!hasElse) return "";
-        return "\n" + "\t".repeat(Math.max(0, t)) + "else: " + bodyNode.convertToPython(t+1);
+        if (!hasElse)
+            return "";
+        return "\n" + "\t".repeat(Math.max(0, t)) + "else: " + bodyNode.convertToPython(t + 1);
     }
 
     @Override
     public boolean validateTree(HashMap<String, FunctionDefNode> functionTable, HashMap<String, IdNode> symbolTable) {
         // TODO Auto-generated method stub
-        if(!hasElse){
+        if (!hasElse) {
             return true;
         }
 
         return (bodyNode.validateTree(functionTable, symbolTable));
     }
 
-    public boolean isReturnable(String type){
-        if(hasElse){
+    public boolean isReturnable(String type) {
+        if (hasElse) {
             return bodyNode.isReturnable(type);
         }
         return false;
     }
 
-    
-    
 }
